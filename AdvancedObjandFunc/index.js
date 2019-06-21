0a4bfce202290664ca2d42bd8b89cb2fc892c853
Stefan Gorgevik <stefangorgevik@hotmail.com>
2019-06-21 16:13:10 +0200

    Update

--- AdvancedObjandFunc/index.js
+++ AdvancedObjandFunc/index.js
@@ -1,14 +1,4 @@
-<!doctype html> 
-<html>
-<head>
-        <title></title>
-</head>
-<body>
-
-        
-
-        <script type="text/javascript">
-    //Function constructor
+  //Function constructor
 /*
     var john = {
         name:"John",
@@ -67,10 +57,39 @@ jane.calculateAge();*/
 
 //primitives vs objects
 
+//primitives
+var a = 23;
+var b = a;
+a = 46;
+// console.log(a);
+// console.log(b);
 
 
+//objects
+var obj1 = {
+    name: "John",
+    age: 26
+}     
 
-        </script>
+var obj2 = obj1;
+//they will be the same objects. 
+obj1.age = 30; //changing age on obj1 will do the same on obj2
+// console.log(obj1.age);
+// console.log(obj2.age);
 
-</body>
-</html>
+//functions
+
+var age = 25;
+var obj = {
+    name: "Stefan",
+    city: "Veles"
+}
+
+function change(a ,b) {
+    a = 30;
+    b.city = "Skopje";
+} 
+
+change(age, obj);
+console.log(age);
+console.log(obj.city);
