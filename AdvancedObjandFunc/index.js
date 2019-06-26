1915dd485d47d9833310f6918e71dbd7eae1d3bc
Stefan Gorgevik <stefangorgevik@hotmail.com>
2019-06-26 16:52:43 +0200

    Update index.js

--- AdvancedObjandFunc/index.js
+++ AdvancedObjandFunc/index.js
@@ -79,17 +79,103 @@ obj1.age = 30; //changing age on obj1 will do the same on obj2
 
 //functions
 
-var age = 25;
-var obj = {
-    name: "Stefan",
-    city: "Veles"
+// var age = 25;
+// var obj = {
+//     name: "Stefan",
+//     city: "Veles"
+// }
+
+// function change(a ,b) {
+//     a = 30;
+//     b.city = "Skopje";
+// } 
+
+// change(age, obj);
+// console.log(age);
+// console.log(obj.city);
+
+//First class functions passing functions as arguments
+
+// var years = [1990, 1994, 1965, 1937, 2005, 1998];
+
+// function arrayCalc(arr,fn) {
+//     var arrRes = [];
+//     for(var i = 0; i < arr.length; i++) {
+//        arrRes.push(fn(arr[i])); 
+//     }
+//     return arrRes;
+// }
+
+// function calcAge(el) {
+//     return 2019 - el;
+// }
+
+// function isFullAge(el) {
+//     return el >= 18;
+// }
+
+// var maxHR = (el) => {
+//     if(el >= 18 && el <= 81) {
+//         return Math.round(206.9 - (0.67 * el));
+//     }else {
+//         return -1;
+//     }
+// };
+
+// var ages = arrayCalc(years,calcAge);
+// console.log(ages);
+
+// var fullAges = arrayCalc(ages, isFullAge);
+// console.log(fullAges);
+
+// var hr = arrayCalc(ages, maxHR);
+// console.log(hr);
+
+//FUNCTIONS RETURNING FUNCTIONS
+
+// var interviewQuestion = (job) => {
+//     if(job === "designer") {
+//         return function(name) {
+//             console.log(name + ", can you please explain what UX design is?");
+//         }
+//     } else if (job === "teacher"){
+//         return function(name) {
+//             console.log(name + ", what subject do you teach?");
+//         }
+//     } else {
+//         return function(name) {
+//             console.log("Hello " + name + ", what do you do? ");
+//         }
+//     }
+// } 
+
+// var teacherQuestion = interviewQuestion("teacher"); //THIS becomes a function becase we passed "teacher"
+// teacherQuestion("Stefan"); // calling the func with name
+
+// var designerQuestion = interviewQuestion("designer");
+// designerQuestion("John");
+
+// interviewQuestion("teacher")("Stef");
+
+
+//immediately invoked func expressions IIFE
+
+function game() {
+    var score = Math.random() * 10;
+    console.log(score>= 5);
 }
+game();
 
-function change(a ,b) {
-    a = 30;
-    b.city = "Skopje";
-} 
+(
+    function() {
+        var score = Math.random() * 10;
+        console.log(score>= 5);  
+    }
+)();
 
-change(age, obj);
-console.log(age);
-console.log(obj.city);
+(
+    function(goodLuck) {
+        var score = Math.random() * 10;
+        console.log(score>= 5 - goodLuck);  
+    }
+)(5);
