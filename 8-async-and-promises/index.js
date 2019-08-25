c73f8a4e1530f2e099490e0850d69c9337ce1351
Stefan Gorgevik <stefangorgevik@hotmail.com>
2019-08-26 00:11:03 +0200

    Promises

---
+++ 8-async-and-promises/index.js
@@ -0,0 +1,72 @@
+// const second = () => {
+//     setTimeout(() => {
+//         console.log("From the second func!");
+//     },2000)
+// }
+
+// const first = () => {
+//     console.log("Hey there");
+//     second();
+//     console.log("The end!");
+// }
+// first();
+
+// const getRecipe = () => {
+//     setTimeout(()=> {
+//         const recipeID = [523, 883, 432, 974];
+//         console.log(recipeID)
+
+//         setTimeout(id=> {
+//             const recipe = {title: "Fresh tomato pasta" , publisher: "Stefan"};
+//             console.log(`${id}: ${recipe.title}`);
+
+//             setTimeout(publisher => {
+//                 const recipe = {title: "Italian Piza", publisher: "Jonas"};
+//                 console.log(recipe);
+//             }, 1500, recipe.publisher)
+//         }, 1500, recipeID[2]);
+//     }, 1500);
+// }
+
+// getRecipe();
+
+const getIDs = new Promise((resolve, reject) => {
+    setTimeout(() => {
+        resolve([523, 883, 432, 974]);
+    }, 1500)
+});
+
+const getRecipe = recID => {
+    return new Promise((resolve, reject) => {
+        setTimeout(ID => {
+            const recipe = {title: "Fresh tomato pasta" , publisher: "Stefan"};
+            resolve(`${ID}: ${recipe.title}`);
+        },1500, recID);
+    });
+};
+
+const getRelated = publisher => {
+    return new Promise((resolve, reject) => {
+        setTimeout(pub => {
+            const recipe = {title: "Italian Piza", publisher: "Jonas"};
+            resolve(`${pub}: ${recipe.title}`);
+        }, 1500, publisher)
+    })
+}
+
+getIDs
+.then((IDs) => {
+    console.log("Then " + IDs);
+    return getRecipe(IDs[2]);
+})
+.then(recipe => {
+    console.log(recipe);
+    return getRelated("Jonas");
+})
+.then(recipe => {
+    console.log(recipe);
+    
+})
+.catch(err => {
+    console.log("Error!");
+})
